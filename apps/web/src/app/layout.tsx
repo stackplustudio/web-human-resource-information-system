e95f618d1b89f23edf6ca4f991185b4cd00f0212
budichyn2003 <budigubernur15@gmail.com>
2026-08-13 12:30:59 +0700

    a

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast"; // TAMBAHKAN IMPORT INI
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
